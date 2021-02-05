b4aa48041520d81c0d3a852b292cf594208a7c80
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-05 17:00:02 +0100

    tot opdracht3 is een soort van oplossing gemaakt

--- README.md
+++ README.md
@@ -90,3 +90,10 @@ om onderdelen van de data te formatten. Deze "tv-generator-functie" verwacht Ã©Ã
 Maak drie knoppen op de pagina: `Sorteer op prijs`, `AmbiLight TV's` en `Uitverkochte exemplaren`. Gebruik de code die je in opdracht 2b, 2c en 2d hebt gemaakt en schrijf dit om naar functies zodat je ze kunt aanroepen
 op het moment dat de buttons geklikt worden. Zorg ervoor dat de functies de uitkomsten in de de console loggen als de gebruiker op de bijbehorende knop klikt.
 _Tip_: Google eens naar het `onclick` event van buttons!
+
+// Hoeveel televiesies moeten we in totaal nog verkopen?
+// nog te verkopen: originalStock - sold
+// map? -> voor de hele array?
+// filter?
+// find?
+//sort?
--- main.js
+++ main.js
@@ -161,3 +161,160 @@ const inventory = [
     sold: 8,
   },
 ];
+// #### Opdracht 1
+// * **Opdracht 1a:** Hoeveel exemplaren moeten we nog verkopen? Schrijf een functie die dit berekent.
+
+// - [ ] functie maken
+// - [ ] aanroepen
+// - [ ] parameter toevoegen (inventory)
+// - [ ] inventory ingeven als argument
+// - [ ] count variabele (buiten de loop)
+// - [ ] loop maken (voor elke televisie)
+// - [x] originalStock - sold optellen bij count
+// - [x] Totaal return uit de functie
+
+const countInventoryToSell = inventory.map((inventory) => {
+  return inventory.originalStock - inventory.sold;
+});
+console.log(countInventoryToSell);
+
+const totalInventoryToSell = countInventoryToSell.reduce((a, b) => a + b, 0);
+// return totalInventoryToSell;
+console.log(totalInventoryToSell);
+
+// * **Opdracht 1b:** Zorg ervoor dat dit aantal _in het rood_ wordt weergegeven op de pagina
+// const soldTV = document.getElementById("soldTV");
+// const soldTV = document.getElementById("soldTV").textContent = totalInventoryToSell; // komt niet in rode inputveld terecht
+
+
+// #### Opdracht 2
+// Opdracht 2a: Gebruik een array-methode om een array te maken met alle tv-type namen.
+// MAP -> ['fancy samsung', 'fancier samsung', 'lg big screen']
+// - [x] maak variabele voor de uitkomst
+// - [x] inventory.map()
+// - [x] geef een functie aan map als argument
+// - [x] voeg de parameter toe (televisie, product)
+// - [x] zoeken naar de juiste property
+// - [x] return de juiste property
+// - [x] log het resultaat
+
+const allTvName = inventory.map((inventory) => {
+  return inventory.name;
+});
+console.log(allTvName);
+
+// Opdracht 2b: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn.
+
+// FILTER ->
+// - [x] maak variabele voor de uitkomst
+// - [x] inventory.filter()
+// - [x] geef een functie aan map als argument
+// - [x] voeg de parameter toe (televisie, product)
+// - [x] zoeken naar de juiste property
+// - [x] check: is de televisie uitverkocht? true of false
+// - [x] return true of false
+// - [x] log het resultaat
+
+const soldTvTypes = inventory.filter((inventory) => {
+ return (inventory.originalStock === inventory.sold); // true of false
+   });
+console.log(soldTvTypes);
+
+const soldTvTypesName = soldTvTypes.map((soldTvType) => {
+  return soldTvType.name; // alleen de namen mappen
+});
+console.log(soldTvTypesName);
+
+// 2c
+//* **Opdracht 2c:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken.
+
+
+const AmbilightTv = inventory.filter((inventory) => {
+  return inventory.ambilight = true; // true of false
+});
+console.log(AmbilightTv);
+
+const AmbilightTvName = AmbilightTv.map((AmbilightTv) => {
+  return AmbilightTv.name; // map alleen de namen van Tv's die ambilight hebben
+});
+console.log(AmbilightTvName);
+
+//* **Opdracht 2d:** Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert.
+
+const TvPrices = inventory.sort((televisieA, televisieB) => {
+  // console.log("hi", televisieA.price, televisieB.price);
+  return televisieA.price - televisieB.price;
+  console.log(difference);
+  //  return difference;
+})
+console.log("sorted?", TvPrices);
+
+// #### Opdracht 3
+// * **Opdracht 3a:** Wat is onze doel-opbrengst?
+
+
+// - [x] maak variabele voor de uitkomst
+// - [x] inventory.map()
+// - [x] geef een functie aan map als argument
+// - [x] voeg de parameter toe (televisie, product)
+// - [x] zoeken naar de juiste property
+// - [x] return de juiste property
+// - [x] log het resultaat
+
+const AimToEarn = inventory.map(() => {
+  return inventory.originalStock * inventory.price;
+});
+console.log(AimToEarn); // ?NaN
+
+const totalAimToEarn = AimToEarn.reduce((a, b) => a + b, 0);
+// return totalInventoryToSell;
+console.log(totalAimToEarn); // ?NaN
+
+//     Bereken wat de totale opbrengst is, als we alle exemplaren van ieder type zouden verkopen. Geef dit in het **blauw** weer op de pagina.
+// const aim = document.getElementById("totalAimToEarn"); // ? werkt niet!!
+
+
+// * **Opdracht 6b:** Hoeveel hebben we tot nu toe verdient?
+//     Bereken hoeveel we tot nu toe verdient hebben met het aantal verkochte tv's. Geef dit weer in het **groen** weer op de pagina
+const totalEarned = inventory.map(() => {
+  return inventory.sold * inventory.price
+});
+console.log(totalEarned);
+
+
+// #### Opdracht 4
+// Geef de type-namen van **twee** tv's weer op de pagina. Welke tv's dat precies zijn, maakt niet zoveel uit.
+//     Voor nu betekent dit dat je het appenden van de nodes twee keer moet uitschrijven, dat is niet erg!
+//
+//     #### Opdracht 5
+// We gaan één van de twee tv's van de vorige opdracht weergeven in het volgende format:
+//
+//     ```
+//   Philips 43PUS6504/12 - 4K TV
+//   €379,-
+//   43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)
+//
+//   NIKKEI NH3216SMART - HD smart TV
+//   €159,-
+//   32 inch (81 cm)
+//   ```
+//
+// * **Opdracht 5a:** Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv.
+//     Maak een functie die één tv-object als parameter verwacht en de naam op de volgende manier samenvoegt: `[merk] [type] - [naam]` zoals `Philips 43PUS6504/12 - 4K TV` of `NIKKEI NH3216SMART - HD smart TV`.
+//     Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken.
+//
+// * **Opdracht 5b:** Zorg ervoor dat de prijs van een tv netjes geformat wordt.
+//     Maak een functie die één tv-prijs als parameter verwacht (zoals `379`) en daar de volgende string van maakt: `€379,-`. Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken.
+//
+// * **Opdracht 5c:** Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv in zowel inches als cm
+// Maak een functie die één screen-sizes array verwacht en de groottes op de volgende manier samenvoegt: `[schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm)` etc.
+//     Dus een input van `[32]` geeft `32 inch (81 cm)` en een input van `[43, 50, 55, 58]` geeft `43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)`. Zorg ervoor dat je deze functie voor iedere tv zou kunnen gebruiken,
+//     zowel voor tv's met maar één schermgrootte als met tientallen schermgroottes.
+//
+// * **Opdracht 5d:** Zorg ervoor de informatie van één van de twee tv's zoals het voorbeeld wordt weergegeven op de pagina.
+// Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
+//
+// * **Opdracht 5e:** Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld.
+// Dit wil je natuurlijk niet acht keer opnieuw schrijven, want nu zijn het 8 tv's, maar in de toekomst misschien wel 200! Gebruik in deze functie de voorgaande functies die je hebt geschreven,
+// om onderdelen van de data te formatten. Deze "tv-generator-functie" verwacht één parameter: de volledige array met tv-objecten. Vergeet 'm niet aan te roepen!
+// //
